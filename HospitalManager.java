5e462ebb4a054182c7376f8faaca057f4c9ff4fa
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-19 15:17:10 +0100

    First commit. Programme for testing Patient class created.

---
+++ HospitalManager.java
@@ -0,0 +1,36 @@
+public class HospitalManager {
+	private Patient patientListStart = null;
+	
+	public static void main(String[] args) {
+		HospitalManager hm = new HospitalManager();
+		hm.launch();
+	}
+	
+	private void launch() {
+		Patient firstPatient = new Patient("Amy", 25, "broken arm");
+		patientListStart = firstPatient;
+		boolean finished = false;
+		while (!finished) {
+			System.out.println("Please enter name of next patient.");
+			System.out.println("Finish by typing 0.");
+			System.out.print("> ");
+			String name = System.console().readLine();
+			if (name.equals("0")) {
+				finished = true;
+				break;
+			} else {
+				System.out.println("Please enter age of this patient.");
+				System.out.print("> ");
+				String s = System.console().readLine();
+				int age = Integer.parseInt(s);
+				System.out.println("Please enter illness of this patient.");
+				System.out.print("> ");
+				String illness = System.console().readLine();
+				Patient nextPatient = new Patient(name, age, illness);
+				patientListStart.addPatient(nextPatient);
+			}
+		}
+		System.out.println (" ");
+		patientListStart.printList();
+	}
+}
