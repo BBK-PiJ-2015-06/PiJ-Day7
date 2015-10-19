7a7b22947bcc395cd63d226d0f25e381a1efcdcb
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-19 15:39:34 +0100

    Method for deleting elements added. Tested and works apart from when trying to delete first element.

--- Patient.java
+++ Patient.java
@@ -33,4 +33,14 @@ public class Patient {
 			nextPatient.printList();
 		}
 	}
+	
+	public void deletePatient(String name) {
+		if (this.nextPatient == null) {
+			System.out.println("Patient to remove not found.");
+		} else if (this.nextPatient.name.equals(name)) {
+			this.nextPatient = nextPatient.nextPatient;
+		} else {
+			this.nextPatient.deletePatient(name);
+		}
+	}
 }
