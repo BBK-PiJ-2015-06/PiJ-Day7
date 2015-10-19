dff989398eb0426574bfed4017ddce17d5016db6
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-19 15:16:19 +0100

    First commit. Methods for adding new elements and printing all elements tested and working correctly.

---
+++ Patient.java
@@ -0,0 +1,36 @@
+public class Patient {
+	
+	private String name;
+	private int age;
+	private String illness;
+	private Patient nextPatient;
+	
+	public Patient(String name, int age, String illness) {
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null;
+	}
+	
+	public void addPatient(Patient newPatient) {
+		if (this.nextPatient == null) {
+			this.nextPatient = newPatient;
+		} else {
+			this.nextPatient.addPatient(newPatient);
+		}
+	}
+	
+	public void printElement() {
+		System.out.println("Name: " + this.name);
+		System.out.println("Age: " + this.age);
+		System.out.println("Illness: " + this.illness);
+		System.out.println(" ");
+	}
+	
+	public void printList() {
+		this.printElement();
+		if(this.nextPatient != null) {
+			nextPatient.printList();
+		}
+	}
+}
