b459b9038e7907223713cc0046c85235fc86b29a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-23 23:52:09 +0100

    Solution for exercise 6 completed.

--- Exercise 6 - Circular Lists/HospitalManager.java
+++ Exercise 6 - Circular Lists/HospitalManager.java
@@ -8,6 +8,8 @@ public class HospitalManager {
 	
 	public static void main(String [] args) {
 		HospitalManager hm = new HospitalManager();
+		
+		//Part 1
 		for(int i = 0; i < 5; i++) {
 			System.out.print("Please provide name of patient: ");
 			String name = System.console().readLine();
@@ -19,6 +21,30 @@ public class HospitalManager {
 			Patient newPatient = new Patient(name, age, illness);
 			hm.addPatient(newPatient);
 		}
+		
+		//Part2
+		hm.printList();
+		
+		//Part3
+		hm.deletePatient();
+		hm.deletePatient();
+		
+		//Part4
+		hm.printList();
+		
+		//Part5
+		System.out.print("Please provide name of patient: ");
+		String name = System.console().readLine();
+		System.out.print("Age: ");
+		int age = Integer.parseInt(System.console().readLine());
+		System.out.print("Illness: ");
+		String illness = System.console().readLine();
+		System.out.println("");
+		Patient newPatient = new Patient(name, age, illness);
+		hm.addPatient(newPatient);
+		hm.deletePatient();
+		
+		//Part6
 		hm.printList();
 	}
 	
@@ -51,5 +77,31 @@ public class HospitalManager {
 			System.out.println("");
 			current = current.getNext();
 		} while(current != startList);
+	}
+	
+	//Method for deleting a patient.
+	private int deletePatient() {
+		if(startList == null) {
+			System.out.println("There are no patients in the list!");
+			return 1;
+		}
+		System.out.print("Name of patient to be deleted: ");
+		String name = System.console().readLine();
+		Patient current = startList;
+		while(current.getNext() != startList) {
+			if(current.getNext().getName().equals(name)) {
+				current.setNext(current.getNext().getNext());
+				return 0;
+			} else {
+				current = current.getNext();
+				if(current.getNext() == startList && current.getNext().getName().equals(name)) {
+					current.setNext(current.getNext().getNext());
+					startList = current.getNext();
+					return 0;
+				}
+			}
+		}
+		System.out.println("There is no patient that matches this name.");
+		return 1;
 	}
 }
